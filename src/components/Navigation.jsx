99779f03756af76d134f9d5a4f432c12feed0493
Lucas <rezaatv@gmail.com>
2019-05-23 16:36:08 +0200

    Design html pour la navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 export default class Navigation extends React.Component {
   render () {
     return (
-      <div>
-        Nav
-      </div>
+      <nav className="navbar navbar-dark bg-dark">
+        <span className="navbar-brand" href="#1">
+          E-shop
+        </span>
+        <span className="nav-item nav-link text-white" href="#2"><strong>( 2 )</strong> article(s) : <strong>12.35 €</strong></span>
+      </nav>
     )
   }
 }
