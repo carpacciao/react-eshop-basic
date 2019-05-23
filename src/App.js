297740a47ca8beebba51d704383b850c82c3f88a
Lucas <rezaatv@gmail.com>
2019-05-23 16:28:53 +0200

    création du component Navigation et son importation dans App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Navigation from './components/Navigation'
+
 class App extends React.Component {
   render () {
     return (
       <div>
-        app
+        <Navigation />
       </div>
     )
   }
