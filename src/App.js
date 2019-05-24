82e1450340a302fc7946235e30da30fe9776bdc8
Lucas <rezaatv@gmail.com>
2019-05-24 09:31:35 +0200

    création du component ShopList et son importation dans App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Navigation from './components/Navigation'
+import ShopList from './components/ShopList'
 
 class App extends React.Component {
   render () {
     return (
       <div>
         <Navigation />
+        <ShopList />
       </div>
     )
   }
---
+++ src/components/ShopList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class ShopList extends React.Component {
+  render () {
+    return (
+      <div>
+        shoplist
+      </div>
+    )
+  }
+}
