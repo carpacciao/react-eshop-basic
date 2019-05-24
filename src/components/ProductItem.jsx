32aa0b1f0e77dc7e9c847797f24076490e10e06f
Lucas <rezaatv@gmail.com>
2019-05-24 10:00:22 +0200

    Transformation des cards en component ProductItem et son importation dans ShopList

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,87 @@
+import React, {Fragment} from 'react'
+
+import dragibus from '../images/dragibus.jpg'
+import grenade from '../images/grenade.jpg'
+import napoleon from '../images/napoleon.jpg'
+import fruitella from '../images/fruitella.jpg'
+
+
+export default class ProductItem extends React.Component {
+  render () {
+    return (
+      <Fragment>
+        <div className="col-4 mt-3">
+          <div className="card">
+            <img src={dragibus} className="card-img-top" alt="something" style={{ maxHeight: '300px' }} />
+            <div className="card-body">
+              <h5 className="card-title">Dragibus</h5>
+              <p className="card-text row">
+                <div className="col-6">
+                  <strong>1.50 €</strong>
+                </div>
+                <div className="col-6">
+                  <strong>( 7 ) en stock</strong>
+                </div>
+              </p>
+              <button className="btn btn-primary">Ajouter au panier</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4 mt-3">
+          <div className="card">
+            <img src={grenade} className="card-img-top" alt="something" style={{ maxHeight: '300px' }} />
+            <div className="card-body">
+              <h5 className="card-title">Grenade</h5>
+              <p className="card-text row">
+                <div className="col-6">
+                  <strong>1.95 €</strong>
+                </div>
+                <div className="col-6">
+                  <strong>( 2 ) en stock</strong>
+                </div>
+              </p>
+              <button className="btn btn-primary">Ajouter au panier</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4 mt-3">
+          <div className="card">
+            <img src={napoleon} className="card-img-top" alt="something" style={{ maxHeight: '300px' }} />
+            <div className="card-body">
+              <h5 className="card-title">Napoleon</h5>
+              <p className="card-text row">
+                <div className="col-6">
+                  <strong>2.49 €</strong>
+                </div>
+                <div className="col-6">
+                  <strong>( 12 ) en stock</strong>
+                </div>
+              </p>
+              <button className="btn btn-primary">Ajouter au panier</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4 mt-3">
+          <div className="card">
+            <img src={fruitella} className="card-img-top" alt="something" style={{ maxHeight: '300px' }} />
+            <div className="card-body">
+              <h5 className="card-title">Fruitella</h5>
+              <p className="card-text row">
+                <div className="col-6">
+                  <strong>0.60 €</strong>
+                </div>
+                <div className="col-6">
+                  <strong>( 21 ) en stock</strong>
+                </div>
+              </p>
+              <button className="btn btn-primary">Ajouter au panier</button>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    )
+  }
+}
--- src/components/ShopList.jsx
+++ src/components/ShopList.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import dragibus from '../images/dragibus.jpg'
-import grenade from '../images/grenade.jpg'
-import napoleon from '../images/napoleon.jpg'
-import fruitella from '../images/fruitella.jpg'
+import ProductItem from './ProductItem'
 
 export default class ShopList extends React.Component {
   render () {
@@ -10,77 +7,7 @@ export default class ShopList extends React.Component {
       <div className="container mt-5">
         <div className="row">
 
-          <div className="col-4 mt-3">
-            <div className="card">
-              <img src={dragibus} className="card-img-top" alt="something" style={{maxHeight: '300px'}}/>
-                <div className="card-body">
-                  <h5 className="card-title">Dragibus</h5>
-                  <p className="card-text row">
-                    <div className="col-6">
-                      <strong>1.50 €</strong>
-                    </div>
-                    <div className="col-6">
-                      <strong>( 7 ) en stock</strong>
-                    </div>
-                  </p>
-                  <button className="btn btn-primary">Ajouter au panier</button>
-                </div> 
-            </div>
-          </div>
-
-          <div className="col-4 mt-3">
-            <div className="card">
-              <img src={grenade} className="card-img-top" alt="something" style={{maxHeight: '300px'}}/>
-                <div className="card-body">
-                  <h5 className="card-title">Grenade</h5>
-                  <p className="card-text row">
-                    <div className="col-6">
-                      <strong>1.95 €</strong>
-                    </div>
-                    <div className="col-6">
-                      <strong>( 2 ) en stock</strong>
-                    </div>
-                  </p>
-                  <button className="btn btn-primary">Ajouter au panier</button>
-                </div> 
-            </div>
-          </div>
-
-          <div className="col-4 mt-3">
-            <div className="card">
-              <img src={napoleon} className="card-img-top" alt="something" style={{maxHeight: '300px'}}/>
-                <div className="card-body">
-                  <h5 className="card-title">Napoleon</h5>
-                  <p className="card-text row">
-                    <div className="col-6">
-                      <strong>2.49 €</strong>
-                    </div>
-                    <div className="col-6">
-                      <strong>( 12 ) en stock</strong>
-                    </div>
-                  </p>
-                  <button className="btn btn-primary">Ajouter au panier</button>
-                </div> 
-            </div>
-          </div>
-
-          <div className="col-4 mt-3">
-            <div className="card">
-              <img src={fruitella} className="card-img-top" alt="something" style={{maxHeight: '300px'}}/>
-                <div className="card-body">
-                  <h5 className="card-title">Fruitella</h5>
-                  <p className="card-text row">
-                    <div className="col-6">
-                      <strong>0.60 €</strong>
-                    </div>
-                    <div className="col-6">
-                      <strong>( 21 ) en stock</strong>
-                    </div>
-                  </p>
-                  <button className="btn btn-primary">Ajouter au panier</button>
-                </div> 
-            </div>
-          </div>
+          <ProductItem />
 
         </div>
       </div>
